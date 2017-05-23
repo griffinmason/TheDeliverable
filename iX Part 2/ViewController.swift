623e5531e38944f2fb31b698d1cb055b640c27f4
griffinmason <george.mason@centre.edu>
2017-05-23 23:16:07 +0200

    Initial Commit

---
+++ iX Part 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iX Part 2
+//
+//  Created by George Mason on 5/23/17.
+//  Copyright © 2017 Griffin Mason. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
